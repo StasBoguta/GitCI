a523ff2c721e120a8dc983b21ec006c14c5058ca
Stanislav Bohuta <stanislav.bohuta@kingfisher.com>
2021-12-11 13:21:50 +0200

    updated

--- Tests.py
+++ Tests.py
@@ -7,6 +7,9 @@ class TestSum(unittest.TestCase):
     def test_not_empty_students(self):
         self.assertTrue(1 > 0, "Should be not empty database")
 
+    def test_not(self):
+        self.assertTrue(1 > 0, "Should be not empty database")
+
 
 if __name__ == '__main__':
     unittest.main()
